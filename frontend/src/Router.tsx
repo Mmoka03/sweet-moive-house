5f7533918234988eb93c6f41d2f773558f6b338b
Mmoka03 <l55922477@gmail.com>
2021-10-13 10:17:04 +0900

    [modify] run setting

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Router: React.FC = () => (
+    <BrowserRouter>
+        <Switch>
+            {/* <Route exact path="/" component={} /> */}
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Router;
