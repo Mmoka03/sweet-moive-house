ae804214bce78d652a99b82e32f5b954371f11bc
Mmoka03 <l55922477@gmail.com>
2021-10-26 16:34:11 +0900

    [modify] spring boot linking

---
+++ sweetmovie/src/main/java/com/simple/sweetmovie/SweetmovieApplication.java
@@ -0,0 +1,13 @@
+package com.simple.sweetmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetmovieApplication.class, args);
+	}
+
+}
---
+++ sweetmovie/src/test/java/com/simple/sweetmovie/SweetmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.sweetmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
